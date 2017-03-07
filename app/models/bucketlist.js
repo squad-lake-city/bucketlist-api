c5c316ccd4147d0c1627e21e230dc57b5723371e
lbreguet <louis-w@live.com>
2017-03-07 17:34:08 -0500

    Modify model
    -bucketlist refactored to show longitude and latitude
    -added virtuals to show what hemisphere the place is in
    -next thing: test code with curl scripts

--- app/models/bucketlist.js
+++ app/models/bucketlist.js
@@ -4,8 +4,30 @@ const mongoose = require('mongoose');
 
 const bucketlistSchema = new mongoose.Schema({
   location: {
-    type: String,
-    required: true,
+    longitude: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function(num) {
+        if (num <= 180 && num >= -180) {
+          return num;
+        }
+      },
+      message: 'Must be between -90 and 90'
+      }
+    },
+    latitude: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function(num) {
+        if (num <= 90 && num >= -90) {
+          return num;
+        }
+      },
+      message: 'Must be between -90 and 90'
+      }
+    }
   },
   activity: {
     type: String,
@@ -14,6 +36,14 @@ const bucketlistSchema = new mongoose.Schema({
   complete: {
     type: Number,
     required: true,
+    validate: {
+      validator: function(num) {
+        if (num === 0 && num === 1) {
+          return num;
+        }
+      },
+      message: 'Please check one of the boxes'
+    }
   },
   description: {
     type: String,
@@ -27,13 +57,52 @@ const bucketlistSchema = new mongoose.Schema({
 }, {
   timestamps: true,
   toJSON: {
-    virtuals: false,
+    virtuals: true,
     transform: function (doc, ret, options) {
       let userId = (options.user && options.user._id) || false;
       ret.editable = userId && userId.equals(doc._owner);
       return ret;
     },
   },
+  toObject: {
+    virtuals: true
+  }
+});
+
+bucketlistSchema.virtual('isNorthernHemisphere?').get(function() {
+  let equator = 0;
+  if (this.latitude > equator) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+bucketlistSchema.virtual('isSoutherHemisphere?').get(function() {
+  let equator = 0;
+  if (this.latitude < equator) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+bucketlistSchema.virtual('isWesternHemisphere?').get(function() {
+  let pole = 0;
+  if (this.longitude < pole) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+bucketlistSchema.virtual('isEasternHemisphere?').get(function() {
+  let pole = 0;
+  if (this.longitude > pole) {
+    return true;
+  } else {
+    return false;
+  }
 });
 
 const Bucketlist = mongoose.model('Bucketlist', bucketlistSchema);
