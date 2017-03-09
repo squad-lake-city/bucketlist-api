05aae90fddef723898b364dcfde3cf790862c8dc
lbreguet <louis-w@live.com>
2017-03-09 15:07:15 -0500

    Model change
    -refactored bucketlist model to just have name and activity
    -deleted google model because we only a name, not storing data from
     google

--- app/models/bucketlist.js
+++ app/models/bucketlist.js
@@ -5,39 +5,11 @@ const mongoose = require('mongoose');
 const bucketlistSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-  },
-  lng: {
-    type: Number,
-    required: true,
-  },
-  lat: {
-    type: Number,
-    required: true,
-  },
-  place_id: {
-    type: String,
     required: true
   },
   activity: {
     type: String,
     required: true,
-  },
-  complete: {
-    type: Number,
-    required: true,
-    validate: {
-      validator: function(num) {
-        if (num === 0 && num === 1) {
-          return num;
-        }
-      },
-      message: 'Please check one of the boxes'
-    }
-  },
-  description: {
-    type: String,
-    required: false,
   },
   _owner: {
     type: mongoose.Schema.Types.ObjectId,
@@ -46,11 +18,9 @@ const bucketlistSchema = new mongoose.Schema({
   },
 }, {
   timestamps: true,
-
-
   toJSON: {
     virtuals: false,
-    transform: function (doc, ret, options) {
+    transform: function(doc, ret, options) {
       let userId = (options.user && options.user._id) || false;
       ret.editable = userId && userId.equals(doc._owner);
       return ret;
